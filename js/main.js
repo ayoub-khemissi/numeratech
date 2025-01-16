c90c36d45efa46678f8f9bd2f87dd612679e06d8
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-01-16 21:56:54 +0100

    feat: Header click outside hide burger nav

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ document.getElementById("year").textContent = year;
 document.getElementById("burger-button").addEventListener("click", function() {
     document.getElementById("mobile-menu").classList.toggle("hidden");
 });
+
+document.addEventListener("click", function(event) {
+    if (!event.target.closest("header")) {
+        document.getElementById("mobile-menu").classList.add("hidden");
+    }
+});
