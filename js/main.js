42f3b0305781a3173ab06f93bfcd81e22aa4b2a0
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-01-16 21:21:16 +0100

    feat: All pages rework header responsiveness

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
+
+document.getElementById("burger-button").addEventListener("click", function() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+});
--- js/tailwind.js
+++ js/tailwind.js
@@ -1,13 +1,25 @@
-const dropdown = document.querySelector("#expertise-dropdown");
-const dropdownContent = dropdown.querySelector("#expertise-dropdown-content");
+const expertiseDropdown = document.querySelector("#expertise-dropdown");
+const expertiseDropdownContent = expertiseDropdown.querySelector("#expertise-dropdown-content");
+
+const expertiseDropdownMobile = document.querySelector("#expertise-dropdown-mobile");
+const expertiseDropdownContentMobile = expertiseDropdownMobile.querySelector("#expertise-dropdown-content-mobile");
 
 document.addEventListener("click", (event) => {
-    if (!dropdown.contains(event.target)) {
-        dropdownContent.classList.add("hidden");
+    if (!expertiseDropdown.contains(event.target)) {
+        expertiseDropdownContent.classList.add("hidden");
+    }
+
+    if (!expertiseDropdownMobile.contains(event.target)) {
+        expertiseDropdownContentMobile.classList.add("hidden");
     }
 });
 
-dropdown.addEventListener("click", (event) => {
+expertiseDropdown.addEventListener("click", (event) => {
     event.stopPropagation();
-    dropdownContent.classList.toggle("hidden");
+    expertiseDropdownContent.classList.toggle("hidden");
+});
+
+expertiseDropdownMobile.addEventListener("click", (event) => {
+    event.stopPropagation();
+    expertiseDropdownContentMobile.classList.toggle("hidden");
 });
