2e57ed9afb724f0b6d77c1ac77fd2866b96fb516
Ayoub KHEMISSI <akhemissi@norsys.fr>
2025-02-01 19:47:46 +0100

    chore: format

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
 
-document.getElementById("burger-button").addEventListener("click", function() {
+document.getElementById("burger-button").addEventListener("click", function () {
     document.getElementById("mobile-menu").classList.toggle("hidden");
 });
 
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     if (!event.target.closest("header")) {
         document.getElementById("mobile-menu").classList.add("hidden");
     }
